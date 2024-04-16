5e77328bf33e8f6a9ba5feeeb38751ce1970100d
Keerthana <surji73@gmail.com>
2024-04-16 12:21:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -16,9 +16,7 @@ function App() {
      
         <Routes>
           <Route path="/" element={<Login />} />
-          {/* <Route path="/send-email" element={<EmailForm />} /> */}
           <Route path="/send-email" element={<ProtectedRoute><EmailForm /></ProtectedRoute>} />
-          {/* <Route path="/send-email" element={<EmailForm />} /> */}
           <Route path="/register" element={<Register />} />
         </Routes>
      
